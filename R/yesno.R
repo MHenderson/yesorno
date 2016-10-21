f2329d5a754907f9985d27d49ad75a956154916b
Matthew Henderson <matthew.james.henderson@gmail.com>
2016-10-21 11:44:30 -0400

    Bug fix!

--- R/yesno.R
+++ R/yesno.R
@@ -3,7 +3,7 @@
 #' @return can be yes, no or maybe.
 #' @export
 
-yesno <- function(force = "no"){
+yesno <- function(force = FALSE){
   url <- paste0("https://www.yesno.wtf/api?force=", force)
   return(jsonlite::fromJSON(url))
 }
