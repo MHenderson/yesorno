9d807f01db993a8c10935454b85e4d623d34d76c
Matthew Henderson <matthew.james.henderson@gmail.com>
2016-10-25 16:29:50 -0400

    Extend unit testing.

--- tests/testthat/test-yesno.R
+++ tests/testthat/test-yesno.R
@@ -1,7 +1,15 @@
-context("force yes")
+context("forcing")
 
 test_that("forcing yes works", {
 
   expect_equal(yesno::yesno(force = "yes")$answer, "yes")
+  expect_equal(yesno::yesno(force = "yes")$forced, TRUE)
+
+})
+
+test_that("forcing no works", {
+
+  expect_equal(yesno::yesno(force = "no")$answer, "no")
+  expect_equal(yesno::yesno(force = "no")$forced, TRUE)
 
 })
