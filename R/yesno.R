3720cd983b0b36dddf24a82e94cf77201a02e7cb
Matthew Henderson <matthew.james.henderson@gmail.com>
2016-10-25 11:01:06 -0400

    The default value of force is NULL.

--- R/yesno.R
+++ R/yesno.R
@@ -3,7 +3,7 @@
 #' @return can be yes, no or maybe.
 #' @export
 
-yesno <- function(force = FALSE){
+yesno <- function(force = NULL){
   url <- paste0("https://www.yesno.wtf/api?force=", force)
   return(jsonlite::fromJSON(url))
 }
